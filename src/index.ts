3abddaab2bb6eb65e5c3dd36452b71303f06a373
Julien-Broyard <broyard.dev@gmail.com>
2019-02-04 16:15:30 +0100

    :heavy_minus_sign: Remove graphql-query-complexity

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ import cors from "cors";
 import "dotenv/config";
 import Express from "express";
 import session from "express-session";
-import queryComplexity, {
-  fieldConfigEstimator,
-  simpleEstimator
-} from "graphql-query-complexity";
 import helmet from "helmet";
 import { join } from "path";
 import "reflect-metadata";
@@ -22,82 +18,62 @@ import { redis } from "./redis";
 import { Logger } from "./utils/Logger";
 
 const main = async () => {
-  // Connect to database
-  await createConnection();
+	// Connect to database
+	await createConnection();
 
-  // BuildSchema
-  const schema = await buildSchema({
-    resolvers: [join(__dirname, "/modules/**/*.ts")]
-  });
+	// BuildSchema
+	const schema = await buildSchema({
+		resolvers: [join(__dirname, "/modules/**/*.ts")]
+	});
 
-  // Create ApolloServer
-  const apolloServer = new ApolloServer({
-    context: ({ req, res }: any) => ({ req, res }),
-    formatError: formatArgumentValidationError,
-    schema,
-    validationRules: [
-      queryComplexity({
-        createError: (max: number, actual: number) => {
-          return new GraphQLError(
-            `La requête est trop complexe: ${actual}. La complexité maximale autorisée est de : ${max}`
-          );
-        },
-        estimators: [
-          fieldConfigEstimator(),
-          simpleEstimator({
-            defaultComplexity: 1
-          })
-        ],
-        maximumComplexity: 8,
-        onComplete: (complexity: number) => {
-          Logger.info("Complexité de la requête:", complexity);
-        },
-        variables: {}
-      }) as any
-    ]
-  });
+	// Create ApolloServer
+	const apolloServer = new ApolloServer({
+		context: ({ req, res }: any) => ({ req, res }),
+		formatError: formatArgumentValidationError,
+		schema
+	});
 
-  // Init Express
-  const app = Express();
+	// Init Express
+	const app = Express();
 
-  // Init RedisStore
-  const RedisStore = connectRedis(session);
+	// Init RedisStore
+	const RedisStore = connectRedis(session);
 
-  const { SECRET } = process.env;
+	const { SECRET } = process.env;
 
-  // Express Middlewares
-  app.use(
-    cors({
-      credentials: true,
-      origin: "http://localhost:3000"
-    })
-  );
-  app.use(helmet());
-  app.use(compression());
-  app.use(
-    session({
-      cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
-        secure: process.env.NODE_ENV === "production"
-      },
-      name: "qid",
-      resave: false,
-      saveUninitialized: false,
-      secret: SECRET ? SECRET : "thisissupersecret",
-      store: new RedisStore({
-        client: redis as any
-      })
-    })
-  );
+	// Express Middlewares
+	app.use(
+		cors({
+			credentials: true,
+			origin: "http://localhost:3000"
+		})
+	);
+	app.use(helmet());
+	app.use(compression());
+	app.use(
+		session({
+			cookie: {
+				httpOnly: true,
+				maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+				secure: process.env.NODE_ENV === "production"
+			},
+			name: "qid",
+			resave: false,
+			saveUninitialized: false,
+			secret: SECRET ? SECRET : "thisissupersecret",
+			store: new RedisStore({
+				client: redis as any
+			})
+		})
+	);
 
-  // Apply middleware
-  apolloServer.applyMiddleware({ app, cors: false });
+	// Apply middleware
+	apolloServer.applyMiddleware({ app, cors: false });
 
-  // Listen ton PORT 4000
-  app.listen(4000, () => {
-    Logger.info("Express server started on http://localhost:4000/graphql");
-  });
+	// Listen ton PORT 4000
+	app.listen(4000, () => {
+		Logger.info("Express server started on http://localhost:4000/graphql");
+	});
 };
 
 main();
